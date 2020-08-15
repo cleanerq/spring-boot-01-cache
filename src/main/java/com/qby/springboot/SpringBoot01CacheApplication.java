1f7777932c8e01a39a8410f6c8f1e19ea0e29530
cleanerq <qbyun130@163.com>
2020-08-15 19:09:12 +0800

    import

--- src/main/java/com/qby/springboot/SpringBoot01CacheApplication.java
+++ src/main/java/com/qby/springboot/SpringBoot01CacheApplication.java
@@ -3,9 +3,11 @@ package com.qby.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @MapperScan(value = "com.qby.springboot.mapper")
 @SpringBootApplication
+@EnableCaching
 public class SpringBoot01CacheApplication {
 
     public static void main(String[] args) {
