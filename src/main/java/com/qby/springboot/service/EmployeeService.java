62fb71a019242137873806958fa83f238fa1c2af
cleanerq <qbyun130@163.com>
2020-08-15 20:47:51 +0800

    import

--- src/main/java/com/qby/springboot/service/EmployeeService.java
+++ src/main/java/com/qby/springboot/service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.qby.springboot.bean.Employee;
 import com.qby.springboot.mapper.EmployeeMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,6 +18,13 @@ public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    /**
+     * 将方法的运行结果进行缓存，以后再要相同的数据，
+     * 直接从缓存中获取，不用调用方法
+     * @param id
+     * @return
+     */
+    @Cacheable(cacheNames = {"emp"})
     public Employee getEmp(Integer id) {
         log.info("查询:{}号员工", id);
         return employeeMapper.getEmpById(id);
