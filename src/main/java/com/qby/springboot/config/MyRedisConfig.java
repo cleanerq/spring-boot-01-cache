50f9b9d5b94077c7d18f377de708ca4153a7c73e
cleanerq <qbyun130@163.com>
2020-08-22 17:03:43 +0800

    import

--- src/main/java/com/qby/springboot/config/MyRedisConfig.java
+++ src/main/java/com/qby/springboot/config/MyRedisConfig.java
@@ -4,11 +4,16 @@ import com.qby.springboot.bean.Employee;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.net.UnknownHostException;
+import java.time.Duration;
 
 @Configuration
 public class MyRedisConfig {
@@ -21,5 +26,16 @@ public class MyRedisConfig {
         template.setConnectionFactory(redisConnectionFactory);
         template.setDefaultSerializer(new Jackson2JsonRedisSerializer(Employee.class));
         return template;
+    }
+
+    @Bean
+    public RedisCacheManager employeeCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofDays(1))   // 设置缓存过期时间为一天
+                        .disableCachingNullValues()     // 禁用缓存空值，不缓存null校验
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()));     // 设置CacheManager的值序列化方式为json序列化，可加入@Class属性
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(cacheConfiguration).build();     // 设置默认的cache组件
     }
 }
